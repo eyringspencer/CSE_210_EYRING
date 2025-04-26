0919d02fe3202c39fe15a2b143e599efee272368
eyringspencer <eyr18003@byui.edu>
2025-04-26 16:24:22 -0600

    Prove: Dev pt. 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 class Program
 {
@@ -12,26 +14,83 @@ class Program
         string input = Console.ReadLine();
         int grade = int.Parse(input);
 
+        // create an empty letter grade variable
+        string letter_grade;
+
         // Determine the letter grade
         if (grade >= 90)
         {
-            Console.WriteLine("Your Letter Grade is A");
+            letter_grade = "A";
         }
         else if (grade >= 80)
         {
-            Console.WriteLine("Your Letter Grade is B");
+            letter_grade = "B";
         }
         else if (grade >= 70)
         {
-            Console.WriteLine("Your Letter Grade is C");
+            letter_grade = "C";
         }
         else if (grade >= 60)
         {
-            Console.WriteLine("Your Letter Grade is D");
+            letter_grade = "D";
         }
         else
         {
-            Console.WriteLine("Your Letter Grade is F");
+            letter_grade = "F";
         }
+
+        // Print the Letter Grade 
+        // QUESTION FOR CLASS: Wouldn't this be a dangerous setup because there's a chance that all of the conditions fail? Wouldn't that make it so that the console write statement is empty? Or does the else take care of that?
+        Console.WriteLine($"Your letter grade is: {letter_grade}");
+
+        // DETERMINE IF THE GRADE IS A PASSING GRADE
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class.");
+
+        }
+        else
+        {
+            Console.WriteLine("You have failed the class. Better luck next time!");
+        }
+
+
+        // STRETCH CHALLENGES
+
+        // calculate the residual after dividing the grade percentage
+        int res = grade % 10;
+
+
+        // declare the sign variable as a string an initialize it to be blank
+        string sign = "";
+
+        // Conditional logic for sign assignment
+        if (grade >= 90)
+        {
+            if (res < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        else if (grade >= 60)
+        {
+            if (res >= 7)
+            {
+                sign = "+";
+            }
+            else if (res < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // print the letter grade with the appropriate sign
+        Console.WriteLine($"Your Specific Letter Grade is: {letter_grade}{sign}");
+
+
+        
+
+        
     }
 }
