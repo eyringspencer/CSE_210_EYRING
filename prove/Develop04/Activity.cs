fdeff0c5e2c0523217908080e681dc2ebb6159bf
eyringspencer <eyr18003@byui.edu>
2025-06-06 15:47:02 -0600

    working save

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,19 @@
+///<summary>
+/// This Activity class will serve as the parent class to the other activity classes
+/// Attributes:
+/// - welcomeText
+///     - stores the name of the activity the user is doing
+/// 
+/// - description
+///     - stores the text that is in the activity description
+/// </summary>
+
+public class Assignemnt
+{
+    // Attributes
+
+    private string _welcomeText; // can't figure out what this is for yet
+
+    private string
+
+}
