ce53700b44d3833b000e07600770de12216119ef
eyringspencer <eyr18003@byui.edu>
2025-06-11 09:09:46 -0600

    Wednesday Push

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -3,17 +3,43 @@
 /// Attributes:
 /// - welcomeText
 ///     - stores the name of the activity the user is doing
-/// 
 /// - description
 ///     - stores the text that is in the activity description
+/// - Duration
+///     - An int that is the number of seconds that the activity will last
+/// - endMessage
+///     - Stores the text that will be used in the ending message
 /// </summary>
 
-public class Assignemnt
+public class Activity
 {
     // Attributes
 
-    private string _welcomeText; // can't figure out what this is for yet
+    private string _welcomeText;
 
-    private string
+    private string _description;
+
+    private int _duration;
+
+    private string _endMessage;
+
+
+    // Constructors 
+
+    // default
+    public Activity(string welcomeText, string description, int duration, string endMessage)
+    {
+        _welcomeText = welcomeText;
+
+        _description = description;
+
+        _duration = duration;
+
+        _endMessage = endMessage;
+    }
+
+
+
+
 
 }
